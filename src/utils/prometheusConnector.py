20bcbbc15e79b480cf9e823eebcd0dafd093d0ce
Patrick Michiels <it@patrickmichiels.de>
2024-05-06 19:59:06 +0200

    prometheusConnector.py: implemented customizable prometheus memory and cpu queries

--- src/utils/prometheusConnector.py
+++ src/utils/prometheusConnector.py
@@ -2,14 +2,16 @@ from prometheus_api_client import PrometheusConnect
 
 class PrometheusConnector:
     def __init__(self):
-        prometheus_url = "http://prometheus:9090"
-        self.prometheusClient = PrometheusConnect(url=prometheus_url)
-        self.cpuQuery30Seconds="avg(rate(container_cpu_usage_seconds_total{container_label_com_docker_swarm_task_name=~'.+'}[30s]))BY(container_label_com_docker_swarm_service_name)*100"
+        _prometheus_url = "http://prometheus:9090"
+        self._prometheusClient = PrometheusConnect(url=_prometheus_url)
+        self._customizable_cpu_query = "avg(rate(container_cpu_usage_seconds_total{{container_label_com_docker_swarm_service_name='{}', container_label_com_docker_swarm_task_name=~'.+'}}[{}])) * 100"
+        self._customizable_memory_query="avg(container_memory_usage_bytes{{container_label_com_docker_swarm_service_name='{}', container_label_com_docker_swarm_task_name=~'.+'}})"
+        self._cpuQuery30Seconds="avg(rate(container_cpu_usage_seconds_total{container_label_com_docker_swarm_task_name=~'.+'}[30s]))BY(container_label_com_docker_swarm_service_name)*100"
 
     def get_all_services(self):
 
-        # Execute proemtheus query.
-        result = self.prometheusClient.custom_query(query=self.cpuQuery30Seconds)
+        # Execute prometheus query.
+        result = self._prometheusClient.custom_query(query=self._cpuQuery30Seconds)
 
         # Return services.
         services = []
@@ -18,16 +20,23 @@ class PrometheusConnector:
         return services
 
 
-    def get_cpu_avg_30seconds_all_services(self):
+    def get_custom_cpu_metric(self, service_name, time_duration):
 
-        # Execute proemtheus query.
-        result = self.prometheusClient.custom_query(query=self.cpuQuery30Seconds)
+        # Execute prometheus query.
+        customized_cpu_query = self._customizable_cpu_query.format(service_name, time_duration)
+        result = self._prometheusClient.custom_query(query=customized_cpu_query)
+        
+        # Return cpu value.
+        return float(result[0]['value'][1]) if result[0]['value'][1] else 0.0
 
-        # Return services.
-        services = []
-        for item in result:
-            service_name = item['metric']['container_label_com_docker_swarm_service_name']
-            cpu_value = float(item['value'][1]) if item['value'][1] else 0.0
-            services.append({'service_name': service_name, 'cpu_value': cpu_value})
-        return services
+
+    def get_custom_memory_metric(self, service_name):
+
+        # Execute prometheus query.
+        customized_memory_query = self._customizable_memory_query.format(service_name)
+        result = self._prometheusClient.custom_query(query=customized_memory_query)
+
+        # Return memory value.
+        return int(result[0]['value'][1]) if result[0]['value'][1] else 0
+
     
